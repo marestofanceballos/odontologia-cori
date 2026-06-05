4a23ae6320d9f3635caf511d2c2583e30a2e7475
Mariana Estofan Ceballos <mariana@example.com>
2026-06-05 11:28:46 -0300

    Proyecto odontologia creado con React y Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>🦷 CORI</h1>
+      <h2>Consultorio Odontológico de Rehabilitación Integral</h2>
+
+      <button>Solicitar Turno</button>
+    </>
+  )
+}
+
+export default App
